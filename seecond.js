8d03237a1ddb8615360b608eec08d9c724c2b8e1
Ohiri Emeka Emmanuel <mjbanks4real@gmail.com>
2018-03-17 10:56:40 +0000

    adding a test file

--- seecond.js
+++ seecond.js
@@ -1,16 +1,16 @@
 // JavaScript Document
 
-/*var my_password="auto_pass";
-var my_email="emeka@kwenu.com";*/
+var my_password="auto_pass";
+var my_email="emeka@kwenu.com";
 
-/*	function login (email,password) {
+	function login (email,password) {
 			if (email==my_email && password==my_password); {
 				alert("welcome back");
 			}else{
 				alert("email or password incorrect!");
 			}
 	}
-	*/
+	
 	
 	function signIn(){
 		var email=document.getElementById('my_email').value;
